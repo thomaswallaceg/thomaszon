f06b6514d662a8a1478081f99b276ec5f125d9a2
Thomas Wallace <thomas@xmartlabs.com>
2020-06-09 17:05:59 -0300

    Route fixes

--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
 router.post('/', async (request, response) => {
   response.header('Access-Control-Allow-Origin', request.headers.origin);
-  request.session.destroy((err) => {
-    if (!err) {
-      response.clearCookie('connect.sid', { path: '/' });
-      response.send('hue');
-    }
-    response.status(400).send('Couldn\'t delete session');
-  });
+  try {
+    await new Promise((resolve, reject) => {
+      request.session.destroy((err) => {
+        if (err) reject();
+        resolve();
+      });
+    });
+    response.clearCookie('connect.sid', { path: '/' }).send('Succesfully logged out');
+  } catch (error) {
+    response.status(400).send('Error logging out');
+  }
 });
 
 module.exports = router;
